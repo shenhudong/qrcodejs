ecd7b7c6d1acee8edaf4b2144b0e297663950e21
wing-kai <cyj9385@hotmail.com>
2018-05-22 16:38:34 +0800

    update

--- index.js
+++ index.js
@@ -1062,7 +1062,7 @@ var Drawing = useSVG ? svgDrawer : !_isSupportCanvas() ? (function () {
   
   // Android 2.1 bug workaround
   // http://code.google.com/p/android/issues/detail?id=5141
-  if (this._android && this._android <= 2.1) {
+  if (this && this._android && this._android <= 2.1) {
       var factor = 1 / window.devicePixelRatio;
         var drawImage = CanvasRenderingContext2D.prototype.drawImage; 
       CanvasRenderingContext2D.prototype.drawImage = function (image, sx, sy, sw, sh, dx, dy, dw, dh) {
